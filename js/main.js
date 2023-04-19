b113acb6228740142053a75931991c4c84fdc77e
GiadaPirone <123646621+GiadaPirone@users.noreply.github.com>
2023-04-19 17:20:05 +0200

    creata img da js

--- js/main.js
+++ js/main.js
@@ -9,8 +9,11 @@ console.log(listaSlide);
 let  btnAvanti = document.getElementById("btnNext");
 let btnIndietro = document.getElementById("btnBack");
 
-
-
+//inserita img da js
+let immagini = document.createElement("img");
+immagini.src = "./img/03.webp";
+document.getElementById("boxSlide").append(immagini);
+immagini.className = "slide dNone";
 
 // variabile di appoggio
 let slideCorrente = 0;
@@ -35,10 +38,32 @@ btnAvanti.addEventListener("click", function(){
         slideCorrente++;
     } else{
         console.log("non va piu avanti di cosi");
-        for (let c = 0; c < listaSlide.length; c++){
-            
-        }
     }
+
+})
+
+btnIndietro.addEventListener("click", function(){
+
+    if ( slideCorrente >0 ){
+        for (let c = 0; c < listaSlide.length; c++) {
+    
+            let slideIncrementata = listaSlide[c];
+        
+            if( c == slideCorrente-1 ){
+                slideIncrementata.classList.remove("dNone");
+                console.log(" slide corrente:" + c + " va visualizzata")
+            } else{
+                slideIncrementata.classList.add("dNone");
+                console.log(" slide corrente:" + c + " va nascosta")
+            }
+        }
+
+        slideCorrente--;
+    } else{
+        console.log("non va piu indietro adi cosi");
+    }
+
+
 
 })
 
